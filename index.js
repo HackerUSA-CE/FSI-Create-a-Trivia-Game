89d8067f792e320e7d432e76baafc8eeb12a02eb
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-15 16:49:20 -0500

    initialize game

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your Code Here
+let userName = window.prompt('Please enter your name:')
+
+for(let i=0; i<questions.length; i++){
+    console.log(questions[i]);
+    let question = questions[i];
+}
