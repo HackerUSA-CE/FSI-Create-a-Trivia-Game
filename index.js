d95a102d2ec57090d156d276ecd44e81423603aa
kraighamrick <kraighamrick@gmail.com>
2023-03-09 10:29:27 -0800

    new question

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 let userName = window.prompt('kraig')
-for (let i = 0; i < questions.legnth; i++){console.log(questions[i])
-
+for (let i = 0; i < questions.legnth; i++)
+{let question = questions[i]
+window.prompt({
+    text: `What is the first book of the Old Testament?
+    A. Leviticus
+    B. Numbers
+    C. Genesis
+    D. Exodus`,
+    correctAnswer: "C"
+}
+)
 }
