0b52d0c48acc4599e9b65b6f3f3efaf7c0b1b7d8
kraighamrick <kraighamrick@gmail.com>
2023-03-09 11:03:33 -0800

    new repo

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-let userName = window.prompt('kraig')
-for (let i = 0; i < questions.legnth; i++)
-{let question = questions[i]
-window.prompt({
-    text: `What is the first book of the Old Testament?
-    A. Leviticus
-    B. Numbers
-    C. Genesis
-    D. Exodus`,
-    correctAnswer: "C"
-}
-)
+let userName = window.prompt('Please enter your name:')
+
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        console.log('Correct')
+    }
 }
