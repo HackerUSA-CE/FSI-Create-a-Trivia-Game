38b7d5e95925e8f03fdafaed4742d8c81eb1ee77
kraighamrick <kraighamrick@gmail.com>
2023-03-09 11:05:11 -0800

    userscore

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let userName = window.prompt('Please enter your name:')
+let userScore = 0
 
 for(let i = 0; i < questions.length; i++){
     let question = questions[i]
     let userAnswer = window.prompt(question.text)
     if(userAnswer === question.correctAnswer){
-        console.log('Correct')
+        userScore = userScore + 10
     }
 }
+
+window.alert('Your score is: '+userScore)
